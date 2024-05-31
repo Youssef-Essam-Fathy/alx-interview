be7798fd47e942e9a4a9a42173b599b726f4e0ff
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-05-31 04:30:44 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module 0-pascal_triangle.py
+In this function it will generate pascal triangle
+"""
 
-""""""
+
+def fact(numb):
+    """this method will return the factorial of the number"""
+    if numb == 0 or numb == 1:
+        return 1
+    else:
+        pro = 1
+        for i in range(1, numb + 1):
+            pro *= i
+        return pro
+
+
+def pascal_triangle(n):
+    try:
+        if n <= 0:
+            return []
+        else:
+            lists = []
+            for i in range(n):
+                row = []
+                for j in range(i + 1):
+                    row.append(fact(i) // (fact(j) * fact(i - j)))
+                lists.append(row)
+            return lists
+    except Exception:
+        return []
